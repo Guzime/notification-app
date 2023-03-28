fa106e798a0759a7071eb6a2b945d4e81c4ad54d
MDUNAEV <DunaevMaxD@gmail.com>
2023-03-28 22:27:17 +0300

    Added TelegramBotService

--- src/main/java/ru/filit/notificationapp/api/TelegramBotService.java
+++ src/main/java/ru/filit/notificationapp/api/TelegramBotService.java
@@ -1,16 +1,16 @@
 package ru.filit.notificationapp.api;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import ru.filit.notificationapp.dto.CommentsRequestDto;
 import ru.filit.notificationapp.dto.IssueRequestDto;
 
-@FeignClient(name = "TelegramBot", url = "${feign.telegramBot.url}")
+@FeignClient(name = "TelegramBot", url = "${feign.telegram-bot.url}")
 public interface TelegramBotService {
-    @PutMapping("/issue")
+    @PostMapping("/issue")
     void sendIssueForTelegram(@RequestBody IssueRequestDto issueRequestDto);
 
-    @PutMapping("/comment")
+    @PostMapping("/comment")
     void sendCommentsForTelegram(@RequestBody CommentsRequestDto commentsRequestDto);
 }
